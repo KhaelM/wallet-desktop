ffb42195e91dc7c8a7f4413b386d5a87c7da0979
michael-openflex <michael.openflex@gmail.com>
2021-02-23 12:21:18 +0300

    ripple effect ok

--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -54,6 +54,27 @@ export default class ResetPassword extends Component {
         })
     }
 
+    createRipple(event) {
+        const button = event.currentTarget;
+
+        const circle = document.createElement("span");
+        const diameter = Math.max(button.clientWidth, button.clientHeight);
+        const radius = diameter / 2;
+
+        circle.style.width = circle.style.height = `${diameter}px`;
+        circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+        circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+        circle.classList.add("ripple");
+
+        const ripple = button.getElementsByClassName("ripple")[0];
+
+        if (ripple) {
+            ripple.remove();
+        }
+
+        button.appendChild(circle);
+    }
+
     render() {
         return (
             <div id="ResetPassword">
@@ -62,9 +83,9 @@ export default class ResetPassword extends Component {
                 </div>
                 <h1>Wali</h1>
                 <form onSubmit={this.modify}>
-                    <div className={this.state.errors.newPass.length ? "form-input" : "form-input invalid"} >
+                    <div className={this.state.errors.newPass.length ? "form-input vertical invalid" : "form-input vertical"} >
                         <label htmlFor="new-password">Nouveau mot de passe</label>
-                        <input onChange={this.onNewPassChange} id="new-password" className={this.state.errors.newPass.length ? "invalid" : ''} name="new-password" type="password" />
+                        <input onChange={this.onNewPassChange} id="new-password" name="new-password" type="password" />
                         <div className="validation-errors">
                             {this.state.errors.newPass.map((error, index) => {
                                 return (
@@ -73,16 +94,18 @@ export default class ResetPassword extends Component {
                             })}
                         </div>
                     </div>
-                    <label htmlFor="confirm-password">Confirmer mot de passe</label>
-                    <input onChange={this.onConfPassChange} id="confirm-password" name="confirm-password" type="password" />
-                    <div className="validation-errors">
-                        {this.state.errors.confPass.map((error, index) => {
-                            return (
-                                <small key={index}>{error}</small>
-                            )
-                        })}
+                    <div className={this.state.errors.confPass.length ? "form-input vertical invalid" : "form-input vertical"}>
+                        <label htmlFor="confirm-password">Confirmer mot de passe</label>
+                        <input onChange={this.onConfPassChange} id="confirm-password" name="confirm-password" type="password" />
+                        <div className="validation-errors">
+                            {this.state.errors.confPass.map((error, index) => {
+                                return (
+                                    <small key={index}>{error}</small>
+                                )
+                            })}
+                        </div>
                     </div>
-                    <input type="submit" value="Modifier" />
+                    <button id="modify-btn" onClick={this.createRipple} className="full-btn openflex-green-bg" type="submit">Modifier</button>
                 </form>
             </div>
         );
